7c22c93ed2a4e0604abd5a19c9b4bac37f4d14ef
vernonranis <60091485+vernonranis@users.noreply.github.com>
2022-01-26 15:43:48 +0800

    updated the dot-files
    updated

--- dot-files-automation.sh
+++ dot-files-automation.sh
@@ -3,8 +3,18 @@
 RED='\033[0;31m'
 NC='\033[0m'  # No Color
 
+echo -e "${RED}Setting up git \n${NC}"
+git config --global user.name "vernonranis"
+git config --global user.email "vernonranis@gmail.com"
+git config --global credential.helper cache
+
 echo -e "${RED}Installing Necessary Packages \n${NC}"
 
-sudo dnf install python39 postgresql-server vim-enhanced  -y
+echo -e "${RED}Installing Python \n${NC}"
+sudo dnf install python39 -y
 
+echo -e "${RED}Installing Postgres \n${NC}"
+sudo dnf install postgresql-server -y
 
+echo -e "${RED}Installing ViM \n${NC}"
+sudo dnf install vim-enhanced -y
